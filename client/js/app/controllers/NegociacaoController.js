d48716d8f70abb21e84edc9e3553094376a83854
Metamorph Dev <42251514+gtonussi@users.noreply.github.com>
2019-05-07 00:05:25 -0300

    Add files via upload

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,17 +7,27 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._ListaNegociacoes);
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
 
     }
     
     adiciona(event) {
         
         event.preventDefault();
+        
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._ListaNegociacoes);
-        this._limpaFormulario();   
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem.texto = "Negociação Adicionada com Sucesso!"
+        this._mensagemView.update(this._mensagem);
+        
+        this._limpaFormulario();
     }
     
     _criaNegociacao() {
